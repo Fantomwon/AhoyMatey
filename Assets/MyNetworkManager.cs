1d2d12f1a4fa39d8094fe1cefdce05dd5bb0535e
DESKTOP-QAIMJQJ\Deerok <derekmcdevitt@gmail.com>
2017-06-14 17:08:00 -0700

    First checkin of AhoyMatey files

---
+++ Assets/MyNetworkManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class MyNetworkManager : NetworkManager {
+
+	public void MyStartHost () {
+		Debug.Log (Time.timeSinceLevelLoad + " Starting Host");
+		StartHost ();
+	}
+
+	public override void OnStartHost () {
+		Debug.Log ( Time.timeSinceLevelLoad + " Host Started");
+	}
+
+	public override void OnStartClient (NetworkClient myClient) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client Start Requested");
+		InvokeRepeating("Waiting", 0.001f, 0.1f);
+	}
+
+	public override void OnClientConnect (NetworkConnection conn) {
+		Debug.Log (Time.timeSinceLevelLoad + " Client Connected to IP: " + conn.address);
+	}
+
+	void Waiting () {
+		Debug.Log(".");
+	}
+}
