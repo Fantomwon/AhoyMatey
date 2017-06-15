80168b6ed2e9412b383215fc5e9d3592873b7abf
DESKTOP-QAIMJQJ\Deerok <derekmcdevitt@gmail.com>
2017-06-14 17:26:49 -0700

    -Added output to show when a client is waiting to connect to the server -Added a cube object with network transform and network identity to test a moving object over the server between two clients

--- Assets/MyNetworkManager.cs
+++ Assets/MyNetworkManager.cs
@@ -15,14 +15,15 @@ public class MyNetworkManager : NetworkManager {
 
 	public override void OnStartClient (NetworkClient myClient) {
 		Debug.Log (Time.timeSinceLevelLoad + " Client Start Requested");
-		InvokeRepeating("Waiting", 0.001f, 0.1f);
+		InvokeRepeating("PrintDots", 0f, 1f);
 	}
 
 	public override void OnClientConnect (NetworkConnection conn) {
+		CancelInvoke();
 		Debug.Log (Time.timeSinceLevelLoad + " Client Connected to IP: " + conn.address);
 	}
 
-	void Waiting () {
+	void PrintDots () {
 		Debug.Log(".");
 	}
 }
